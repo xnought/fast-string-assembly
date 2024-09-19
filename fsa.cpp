6e318f5c021726438f3673cbf30cebfe08818672
xnought <dbertucci6@gatech.edu>
2024-09-19 19:46:10 -0400

    combine to create new objects

--- fsa.cpp
+++ fsa.cpp
@@ -17,11 +17,11 @@ set<string> atomize_string(string str)
 	return s;
 }
 
-void preprocess_string(string *str_ptr)
+void preprocess_string(string &str)
 {
-	for (int i = 0; i < (*str_ptr).length(); ++i)
+	for (int i = 0; i < str.length(); ++i)
 	{
-		(*str_ptr)[i] = tolower((*str_ptr)[i]);
+		str[i] = tolower(str[i]);
 	}
 }
 
@@ -34,6 +34,23 @@ void print_set(set<string> s)
 	cout << "\n";
 }
 
+void naive_assemble_objects(std::set<string> &objects)
+{
+	set<string> new_objects;
+	for (const auto &a : objects)
+	{
+		for (const auto &b : objects)
+		{
+			new_objects.insert(a + b);
+			new_objects.insert(b + a);
+		}
+	}
+	for (const auto &no : new_objects)
+	{
+		objects.insert(no);
+	}
+}
+
 /**
  * Returns
  * 	-1 on ERROR
@@ -43,18 +60,29 @@ int naive_string_assembly(string str)
 {
 	cout << "Naive String Assembly (slow)" << "\n";
 	cout << "Input String: " << str << "\n";
-	preprocess_string(&str);
+	preprocess_string(str);
 	cout << "Preprocessed: " << str << "\n";
 	set<string> atoms = atomize_string(str);
 	cout << "Atoms: ";
 	print_set(atoms);
+
+	//
+	// NAIVE ALGO
+	//
+
+	// 1. history starts as just the atoms
+	set<string> objects = atoms;
+	// 2. Compute all object combinations to create new objects and add to the pool
+	naive_assemble_objects(objects);
+	cout << "objects: ";
+	print_set(objects);
 
 	return -1;
 }
 
 int main()
 {
-	string a = "ABRACADABRA";
+	string a = "ABRACadabRA";
 	int out = naive_string_assembly(a);
 	return 0;
 }
